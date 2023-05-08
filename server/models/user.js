be123c4760f96b9bcaff38e043026732f66f695f
Roland Kiss <kissrolandkristof@gmail.com>
2023-05-08 22:45:28 +0200

    modellek feltöltése

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+      },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+    age: {
+        type: Number,
+        required: true
+      },
+    password: {
+        type: String,
+        required: true
+      },
+    birthdate: {
+        type: Date,
+        required: true
+      }
+  });
+
+module.exports = mongoose.model('User', userSchema);
